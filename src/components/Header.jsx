94e20b09e1d778679aee6d883d756ed59b79aea2
Ronald Jr <rpintojunior@gmail.com>
2021-01-13 15:37:48 -0300

    Criando o componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,12 @@
-// implement Header component here
-//comitt
+import React from 'react';
+
+class Header extends React.Component {
+    render(){
+     return (
+        <header>
+            <h1>Movie Cards Library</h1>
+        </header>
+     )}
+}
+
+export default Header;
