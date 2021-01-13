54ec1926b8bc695d11d9263cbcd8af4fbca0c8bf
rafasysop <rafaelcostamoura@gmail.com>
2021-01-13 14:21:08 -0300

    Implementando o Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
-// implement Header component here
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return <span>Olá Mundo</span>;
+  }
+}
+
+export default Header;
