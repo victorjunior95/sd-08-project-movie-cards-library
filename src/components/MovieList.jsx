9d6af10689f8da01c51739f4e457f4d50ff36d80
Leonardo <lecastro16@gmail.com>
2021-01-13 18:01:49 -0300

    organizando eslin exercicios 1 e 2 v2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class MovieList extends React.Component {
-    render() {
-        return (
-            <header>
-                <h1>Movie Cards Library</h1>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 
 export default MovieList;
