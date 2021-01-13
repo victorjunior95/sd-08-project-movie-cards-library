ddd74a67c20f0be0d64ca6ebee06633e569f46d3
Igor Nascimento <igorln96@gmail.com>
2021-01-13 16:48:25 -0300

    req 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movies={ movie } />) }
+      </div>
+    );
+  }
+}
+
+export default MovieList;
