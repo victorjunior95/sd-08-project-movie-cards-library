2515e882a52974d05e23df0e4a128b9151cca722
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-13 16:22:29 -0300

    Req 04 completed

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
+      const { rating } = this.props;
     return (
-      null
+      <span className="rating">{ rating }</span>
     );
   }
 }
