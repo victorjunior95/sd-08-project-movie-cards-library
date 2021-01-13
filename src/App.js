86c450223b0ef977f20fa45e536b56cd784ed940
Leonardo <lecastro16@gmail.com>
2021-01-13 17:52:28 -0300

    exercicio 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import movies from './data';
+import MovieList from './components/MovieList';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieList movies={movies} />
+    </div>
+  );
 }
 
 export default App;
+
