7b8608181f3c6d43658071a2d705e8e7a0a2160c
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-01-13 20:56:17 -0300

    import proptype

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,9 @@
 // implement MovieList component here
 
 import React, { Component } from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
 
 class MovieList extends Component {
   render() {
