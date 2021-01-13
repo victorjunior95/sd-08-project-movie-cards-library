4f040e2a194e49a9d2b897153d987e317c761725
Edson Araujo <araujo.barros@matematica.ufrj.br>
2021-01-13 18:32:04 -0300

    Requisito 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    return (
+      <div>
+        <img src={ imagePath } alt="teste" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        {/* <Rating rating={ rating} /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
