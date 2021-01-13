e13d7073583ce51b4483c5ff21cf8be3a7116f4c
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-13 20:20:41 -0300

    :rocket: requisito 3,4 pronto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    return <span>{ rating }</span>;
+    return (
+      <div>
+        <span className="rating">{ rating }</span>
+      </div>
+    );
   }
 }
 
