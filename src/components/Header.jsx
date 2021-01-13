5e49743c169300a6af124af28b6fb1b3b9720cc1
Doug-Fernando <doug.fernando95@gmail.com>
2021-01-13 16:45:12 -0300

    Req 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,10 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="movie-card-header">
+        <header className="page-title ">
+          <h1>Movie Cards Library</h1>
+        </header>
       </div>
     );
   }
