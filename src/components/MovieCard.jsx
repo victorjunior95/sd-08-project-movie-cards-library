5ef66e587bc44f334ec32b3ca130e3f426ab574d
Fabio Ferreira <fabio35547@gmail.com>
2021-01-13 19:15:29 -0300

    Requisito 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,13 +6,15 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div className="movie">
-        <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
-      </div>
+      <section className="movie-card-body">
+        <div className="movie-card">
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline>">{storyline}</p>
+          <Rating className="movie-card-rating" rating={ rating } />
+        </div>
+      </section>
     );
   }
 }
