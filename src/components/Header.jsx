992eccf7cdf0c0680559e73ea7b32d9aa38166c4
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-01-13 16:09:02 -0300

    Req 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,18 @@
 // implement Header component here
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
