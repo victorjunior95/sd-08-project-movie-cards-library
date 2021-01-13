35be7b919812f99cbaaa3ff255e4232e5618d135
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-01-13 16:48:22 -0300

    Cria componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <section>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+        <Rating rating={ movie.rating } />
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
