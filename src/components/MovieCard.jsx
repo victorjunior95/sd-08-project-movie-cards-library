7bc80e03a5dcfc4a476a64884015c8520b89b897
Lucas Nascimento <lucasmvnascimento@gmail.com>
2021-01-13 18:05:27 -0300

    fix lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,6 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    // const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     const { movie } = this.props;
     return (
       <div className="movie-card">
