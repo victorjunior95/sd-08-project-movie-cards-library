dc902a499fc64b4fa19b12e44a123d8b22c20258
Doug-Fernando <doug.fernando95@gmail.com>
2021-01-13 17:03:47 -0300

    Req 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+export default class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+export default class MovieList extends Component {
+  render() {
+    const GenerateList = () => {
+      movies.map(movie => <MovieCard key={ movie.title } movie={ movie } />);
+    };
+
+    return (
+      <div>
+        <GenerateList />
+      </div>
+    );
+  }
+}
