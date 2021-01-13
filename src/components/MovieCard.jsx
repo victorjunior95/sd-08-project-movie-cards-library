f775a7918233e19139c3b2db6907db23f9db0402
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-13 16:08:07 -0300

    3.3: renders movie title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,7 @@ class MovieCard extends React.Component {
     return (
       <section>
         <img src={ this.props.movie.imagePath } alt="movie cover" />
+        <h4>{this.props.movie.title}</h4>
       </section>
     );
   }
