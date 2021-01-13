6f45ede884883964b48bf8b63bf1b1684f46d751
victor718 <victorfelipeoliveira1@gmail.com>
2021-01-13 16:09:57 -0300

    iniciando pojeto movie cards library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
 // implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return <h1>Movie Cards Libary</h1>;
+  }
+}
+
+export default Header;
