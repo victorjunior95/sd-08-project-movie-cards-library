f99b5003f29e506cd215b8eec71b813ca17d732a
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-13 15:26:07 -0300

    renders MovieList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import movies from './data';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+
 
 function App() {
+  const movies = movies;
   return (
     <div className="App">
       <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }
