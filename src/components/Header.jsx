4024390eb807a27bae7e1db14e2d362c366d104c
Bruno Wedley <brunowedley27@gmail.com>
2021-01-13 16:17:55 -0300

    :rocket: primeiro requisito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,14 @@
-// implement Header component here
-// alteração para primeiro commit.
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1> Movie Cards Library </h1>
+      </header>
+
+    );
+  }
+}
+
+export default Header;
