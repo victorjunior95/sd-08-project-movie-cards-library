9059eac4b41c84cd52a67ef6fa3d6cd7de75166a
Daniel <marcio.daniel@msn.com>
2021-01-13 17:14:47 -0300

    subindo primeiro requisito

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 
 import App from './App';
-// import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
+import Header from './components/Header';
+//import MovieCard from './components/MovieCard';
 // import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 
