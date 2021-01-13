52a21b34180992afe0c57bcdbf40330d070ad53a
Doug-Fernando <doug.fernando95@gmail.com>
2021-01-13 16:17:55 -0300

    Commit inicial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
 // implement Header component here
+
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
