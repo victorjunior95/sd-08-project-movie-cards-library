d1381c5ae5962e023f5df8d61ba66b4c7b1f6ea3
MEianelli <filipelouzano@gmail.com>
2021-01-13 16:13:29 -0300

    Segunda tentativa de ESLINT

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div className="movie-card-header">
+      <header className="movie-card-header">
         <h1 className="page-title">Movie Cards Library</h1>
-      </div>
+      </header>
     );
   }
 }
