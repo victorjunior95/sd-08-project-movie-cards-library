559e512feb9afc427b500909aa77ba121f943837
Leonardo <lecastro16@gmail.com>
2021-01-13 18:07:02 -0300

    exercicios 1 ao 2 v3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <header>
-        <h1>Movie Cards Library</h1>
+        <h1>XYZ</h1>
       </header>
     );
   }
