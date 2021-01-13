2027a98ad33be02747622435096260be6e6280cb
Ronald Jr <rpintojunior@gmail.com>
2021-01-13 16:05:23 -0300

    Criando o componente Header- corrigido 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ class Header extends React.Component {
     return (
       <header>
         <h1>Movie Cards Library</h1>
-      </header>
-    ); } }
+      </header>);
+  }
+}
 
 export default Header;
