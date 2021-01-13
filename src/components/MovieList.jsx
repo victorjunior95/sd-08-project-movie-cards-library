d1cbce22f042926541f4e40c8d6f5394a51c2898
Lucas Nascimento <lucasmvnascimento@gmail.com>
2021-01-13 15:58:22 -0300

    test lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className='movie-list'>
+        {this.props.movies.map(movie => <MovieCard movie={movie} />)}
+      </div>
+    )
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return <div className='movie-card-rating'>
+      <div className='rating'>{this.props.rating}</div>
+    </div>
+  }
+}
+
+export default Rating;
