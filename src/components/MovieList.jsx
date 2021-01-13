4a9f5391b61b056b37a09c31e4667fe078319ce9
Pedro Amaral <amaralph1995@gmail.com>
2021-01-13 15:39:51 -0300

    Corrige evaluatorjob

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+
+import movies from '../data.js';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default MovieList;
