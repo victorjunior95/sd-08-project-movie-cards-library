190ed5969ee11ea1dfe7f18939cd64f2ce59bc7c
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-01-13 17:47:42 -0300

    req 1 ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
 
+function Header() {
+  return (
+    <header className="Header">
+      <h1>
+        Movie Cards Library
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
