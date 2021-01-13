1f29f0a8537b9dd20de4339b2a744a3fa5c5fbad
rafasysop <rafaelcostamoura@gmail.com>
2021-01-13 14:50:47 -0300

    Implementando o

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return <h1>Ola</h1>;
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const movies = this.props.movies;
+    return movies.map((movie, index) => {
+      return movie + index;
+    });
+  }
+}
+
+export default MovieList;
