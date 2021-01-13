0b137fc9dd610b1a04438597f0f182c3f5d18ae0
albertassi88 <albertassi88@gmail.com>
2021-01-13 17:35:25 -0300

    Criacao dos componentes Header, MovieCard, MovieList, Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+    render() {
+       const { title, subtitle, storyline, imagePath, ratin } = this.props.movie;
+       return (
+           <div>
+            <img src={imagePath}></img>
+            <h4>{title}</h4>
+            <h5>{subtitle}</h5>
+            <p>{storyline}</p>
+            {Rating}
+           </div>
+       )
+
+    }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+       const { movies } = this.props;
+        return (
+            <section>
+                {movies.map(element => {
+                    return <MovieCard mov={ element } key={ element.title } />
+                })}
+            </section>           
+        )       
+    }
+}
+
+export default MovieList;
