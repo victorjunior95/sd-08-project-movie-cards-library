13bacde09b21e83f2d53e6f584823c21eaef135d
wellington <profwellington17@gmail.com>
2021-01-13 17:20:48 -0300

    fix Requisto1 #VQV

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+
 import Header from './components/Header';
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
       <div className="App">
         <Header />
