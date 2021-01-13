5be66aec1ed432f1fb4d47de54edeea2404ba512
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-01-13 16:14:08 -0300

    adicionando requisito 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const {
+      title,
+      subtitle,
+      storyline,
+      rating,
+      imagePath,
+    } = this.props.infoMovie;
+    return <h1>{title}</h1>;
+  }
+}
+
+export default MovieCard;
