9088f68a045d6a673b6149123deda381f5234cc0
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-13 19:29:23 -0300

    :rocket: requisito 3  pronto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+
+    return (
+      <div>
+        <img src={ imagePath } alt=""/>
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating }/>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <section>
+          {movies.map((movie) => <MovieCard key={ movie.title } />)}
+        </section>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
