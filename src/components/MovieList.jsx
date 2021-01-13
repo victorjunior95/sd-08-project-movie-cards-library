cc89e8e93688493cf4560d4fb49e063d58b301d9
Leonardo <lecastro16@gmail.com>
2021-01-13 18:10:24 -0300

    exercicios 1 ao 2 v4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
+    // const { movies } = this.props;
     return (
       <header>
         <h1>XYZ</h1>
