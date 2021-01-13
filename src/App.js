ee3a93ef0f80ac4aaf010b19bb81eba5ef8314e5
Marcela <marcela9908@gmail.com>
2021-01-13 19:01:59 -0300

    Correçao no arquivo MovieCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieCard from './components/MovieCard';
 
 class App extends React.Component {
   render() {
