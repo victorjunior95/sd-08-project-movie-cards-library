b25984880c7b8d0a7c6a53644887a7450975220f
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-13 15:59:21 -0300

    :package: requisito 1.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
 // implement Header component here
+import React from 'react';
+class Header extends React.Component {
+    render(){
+        return <header>Movie Cards</header>
+    }
+}
+
+export default Header;
