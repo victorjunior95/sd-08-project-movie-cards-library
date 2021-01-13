d35d58d35cb549b1dc6efe10a7b812ce6c2ab328
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-13 15:16:37 -0300

    ESLint - Conserta erros

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() { 
+  render() {
     return (
       <div>
         <header>
           <h1>Movie Cards Library</h1>
         </header>
       </div>
-    )
+    );
   }
 }
- 
+
 export default Header;
