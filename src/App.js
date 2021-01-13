db6f70f5e3c4f1fe00369e6ee6e83428f7d4b64b
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-01-13 16:28:57 -0300

    Alterações na estrutura da classe App

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Header from './components/Header';
 import MovieList from './components/MovieList';
 import data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <MovieList movies={data} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieList movies={data} />
+      </div>
+    );
+  }
 }
-
 export default App;
