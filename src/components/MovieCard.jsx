f2c8af6ca849fc97de9beb8f68250c81900be989
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-13 16:34:35 -0300

    adds css classes to elements and components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,12 +5,14 @@ class MovieCard extends React.Component {
   render() {
     const { imagePath, title, subtitle, storyline, rating } = this.props.movie;
     return (
-      <section>
-        <img src={ imagePath } alt="movie cover" />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
+      <section className="movie-card">
+        <section className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt="movie cover" />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={ rating } />
+        </section>
       </section>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-      const { rating } = this.props;
+    const { rating } = this.props;
     return (
       <span className="rating">{ rating }</span>
     );
