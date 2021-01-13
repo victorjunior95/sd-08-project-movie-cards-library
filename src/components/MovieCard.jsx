0c17aa859e1b767c967aded3cadbea2b90254b6f
liisaambrosen <liisa.ambrosen@gmail.com>
2021-01-13 17:39:26 -0300

    requisitos 2 e 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
 // implement MovieCard component here
+import React from 'react';
+
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+
+    return (
+      <section className="movie-card">
+        <img scr={ imagePath } alt="Imagem do filme" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
