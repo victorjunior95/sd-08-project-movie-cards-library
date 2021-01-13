bbb4013cd7654726df42a669cc66e074daa1ce78
jnpefo <jnpefo@gmail.com>
2021-01-13 18:59:12 -0300

    Req 4, tentativa 1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <h4>{ title }</h4>
+        <img src={ imagePath } alt="teste" />
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const [movie] = this.props;
+    return (
+      <MovieCard />
+    );
+  }
+}
+
+export default MovieList;
