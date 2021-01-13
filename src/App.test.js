958a40778171a930bf5aa9c93eac7cf6348a8b08
Ediberto Oliveira <edibertooliveira@aol.com>
2021-01-13 15:23:52 -0300

    created <MovieList/> & <MovieCard requirement 2

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import { mount, shallow } from 'enzyme';
 
 import App from './App';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
+import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 
 let wrapper;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return <div>move</div>;
+  }
+}
+
+export default MovieCard;
