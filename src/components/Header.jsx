6d3e146509adaed3948e7515efc1eafa98a4adb8
v-icto-r <victor130496@gmail.com>
2021-01-13 16:42:41 -0300

    primeiro requisito- correçao

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-class Header extends React.Component{
-    render(){
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
         return (
-            <header>
-              <h1>Movie Cards Library</h1>
-            </header>
+        <header>
+            <h1>Movie Cards Library</h1>
+        </header>
           );
     }
 }
