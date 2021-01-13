9bac72455c77e4c879c96ef17760dd9e5affffb6
Marcela <marcela9908@gmail.com>
2021-01-13 17:04:17 -0300

    Requisito 1: Criaçao componet Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
-// Iniciando o projeto 🚀
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Movie Cards Library</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
