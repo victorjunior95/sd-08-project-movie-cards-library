41225fe97fef92b8ba528de69db9830ed6bb651b
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-01-13 17:05:54 -0300

    Movie Cards Library Project

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,31 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </>
+    );
+  }
+}
+MovieCard.propTypes = {
+  movie: {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }.isRequired,
+};
+export default MovieCard;
