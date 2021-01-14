2fd0cc61f694e38c1188958cf857eb33606e9437
Felip França <felipefrancafj@gmail.com>
2021-01-14 11:18:14 -0300

    REQ-2 Resolved

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import { mount, shallow } from 'enzyme';
 import App from './App';
 import Header from './components/Header';
 // import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
+import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 
 let wrapper;
