b31323d7c1805bf7e9467fdb9d0f64d48cdff625
Thiago Ferreira <thiagocristhian@gmail.com>
2021-01-13 23:27:17 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
+
+import './data.js';
 import Header from './components/Header';
+
+import './App.css';
+import movies from './data.js';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList movies={ movies }/>
       </div>
     );
   }
