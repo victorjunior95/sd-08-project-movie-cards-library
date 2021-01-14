06a6a4c5433afca3e287561f960b99feb611a7f5
wellington <profwellington17@gmail.com>
2021-01-13 21:44:20 -0300

    Requisto2 #VQV

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import movies from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 class App extends React.Component {
+  
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList movie ={movies}/>
       </div>
     );
   }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React from 'react';
+
+
+class MovieCard extends React.Component {
+  render() {
+    const {title, subtitle, storyline, imagePath, rating} = this.props.movie
+    return (
+      <div>
+        <h4>{title}</h4>
+
+
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
