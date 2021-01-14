b8b4130dc7bd85d2f2043e307ab115c4b0cdd4bc
Douglas Ferreira <dodofp.df@gmail.com>
2021-01-14 19:53:27 -0300

    Requisito rating

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 import Header from './components/Header';
 import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
-// import Rating from './components/Rating';
+import Rating from './components/Rating';
 
 let wrapper;
 
@@ -106,7 +106,7 @@ describe('3 - Crie um componente `<MovieCard />`', () => {
 
   it('3.7 - Passe como prop para o componente `<Rating />` o atributo `rating`', () => {
     wrapper = mount(<MovieCard movie={movie} />);
-    const starRating = wrapper.find('Rating');
+    const starRating = wrapper.find(Rating);
 
     expect(starRating.props().rating).toEqual(4.5);
   });
