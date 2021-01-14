f0ae6649b898d2a734890e750d58a07555f1171c
wellington <profwellington17@gmail.com>
2021-01-13 23:00:00 -0300

    fix2  Requisto2 e 5  #VQV

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <MovieList movies= { movies } />
+        <MovieList movies={ movies } />
       </div>
     );
   }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
-        <img src={ imagePath } alt="Card avatar"/>;
+        <img src={ imagePath } alt="Card avatar" />
         <div>
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
