cd5adf88b6c45f00955d19b77bf17ccee62c123e
Leonardo <lecastro16@gmail.com>
2021-01-14 12:00:54 -0300

    requisitos 1 ao 3 v6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
-    render() {
-      const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-      return (
-        <div>
-          <img src={ imagePath } alt="Photos of Games" />
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
-          <Rating rating={ rating } />
-        </div>
-      );
-    }
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+        <img src={ imagePath } alt="Photos of Games" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
   }
+}
 
 MovieCard.propTypes = {
   movie: PropTypes.arrayOf(
@@ -25,8 +25,8 @@ MovieCard.propTypes = {
       storyline: PropTypes.string,
       rating: PropTypes.number,
       imagePath: PropTypes.string,
-    }),
-  )
+    }).isRequired,
+  ),
 };
 
 export default MovieCard;
