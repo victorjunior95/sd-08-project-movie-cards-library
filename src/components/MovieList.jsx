147ed503342e7935a541e6fb51fc23d3e37bfbfc
Blucilha <blucilha.nascimento@gmail.com>
2021-01-13 21:28:33 -0300

    Exercicio 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return movies.map((element) => element.title);
+  }
+}
+MovieList.propTypes = { movies: PropTypes.arrayOf.isRequired };
+
+export default MovieList;
