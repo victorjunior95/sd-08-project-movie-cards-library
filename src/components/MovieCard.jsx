9cd1fcaf2d8ba73fbcaf1a81d538e5a08dd86ad9
Leonardo <lecastro16@gmail.com>
2021-01-14 11:33:14 -0300

    requisitos 1 ao 3 v2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rating from './Rating'
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
-        <img src={ imagePath } alt = 'Image of Movies'></img>
+        <img src={ imagePath } alt = "Photos of Games"/>
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <Rating rating = { rating }/>
+        <Rating rating={ rating }/>
       </div>
     );
   }
@@ -25,8 +25,8 @@ MovieCard.propTypes = {
       storyline: PropTypes.string,
       rating: PropTypes.number,
       imagePath: PropTypes.string,
-    })
-    )
-  }
+    }),
+  )
+}
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    const  { rating } = this.props;
+    const { rating } = this.props;
     return (
       <div>
         {rating}
