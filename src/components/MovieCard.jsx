7c962588bced818b79f6c9deb7395649eff4557c
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-14 06:54:33 -0300

    requisito 2

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// class MovieCard extends React.Component {
+//   render() {
+//     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+//     return (
+//       <section>
+//         <img src={ imagePath } alt="Imagem do filme" />
+//         <h4>{title}</h4>
+//         <h5>{subtitle}</h5>
+//         <p>{storyline}</p>
+//         {/* <div>
+//           <Rating rating={ rating } />
+//         </div> */}
+//       </section>
+//     );
+//   }
+// }
+
+// export default MovieCard;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* <MovieCard /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
