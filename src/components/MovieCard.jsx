aa4d00a1ae8cc9033a121b5bc826b90d2d1c9d6a
Leonardo <lecastro16@gmail.com>
2021-01-14 12:04:47 -0300

    requisitos 1 ao 3 v7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,8 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { movie } = this.props;  
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt="Photos of Games" />
