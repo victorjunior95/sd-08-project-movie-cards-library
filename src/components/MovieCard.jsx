ef7f4163b885e20d6b65cc7441feb9d39739dd91
Daniel <danieldsrd@gmail.com>
2021-01-14 13:02:55 -0300

    ClassName Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,9 @@ class MovieCard extends React.Component {
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
         <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <Rating className="movie-card-rating" rating={ rating } />
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
