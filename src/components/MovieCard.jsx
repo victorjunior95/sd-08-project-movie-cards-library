f3b29c35591885e0a590ae781009b344db4df443
Giovanna Betti <giovannabetti@gmail.com>
2021-01-13 22:27:22 -0300

    Resolvendo requisitos fora de ordem, seguindo a logica usada em aula

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+
+    return (
+      <section>
+        <img src={ imagePath } alt="Cartaz do filme" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <span>
+          Rating
+          {rating}
+        </span>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
