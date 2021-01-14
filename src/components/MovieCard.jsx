03b1d7b00cba18d284dae1004fe2d27bdf88f183
wellington <profwellington17@gmail.com>
2021-01-14 19:32:00 -0300

    Requisto3  #VQV

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -11,8 +12,8 @@ class MovieCard extends React.Component {
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
           <p>{storyline}</p>
-          <h3>{rating}</h3>
         </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
