8d57fa5ab3e4ac9eec1ce85851cc91e60bb8b8a6
victorjunior95 <victorjr.muchacho@gmail.com>
2021-01-14 11:54:07 -0300

    Teste para completar projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,14 +4,11 @@ import PropTypes from 'prop-types';
 class Rating extends Component {
   render() {
     const { rating } = this.props;
-    // const rating = this.props.rating.toString();
-    const ratingString = rating.toString();
-    // console.log(typeof ratingString);
 
     return (
-      <span>
-        { ratingString }
-      </span>
+      <div>
+        <span>{ rating }</span>
+      </div>
     );
   }
 }
