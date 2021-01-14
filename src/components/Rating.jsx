816382a7cbf8629ef4c3977587ec520e53615a16
Leandro Lachini <leandrolachini@gmail.com>
2021-01-14 14:17:32 -0300

    projeto finalizado_correçao codigo_1

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -8,14 +8,14 @@ class Rating extends React.Component {
     const { rating } = this.props;
     return (
       <div className="rating">
-        <span className="movie-card-rating">{ rating }</span>      
+        <span className="movie-card-rating">{ rating }</span>
       </div>
     );
   }
 }
 
 Rating.propTypes = {
-  rating: PropTypes.number,
+  rating: PropTypes.number.isRequired,
 };
 
 
