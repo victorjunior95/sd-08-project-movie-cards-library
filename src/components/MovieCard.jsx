d439cf5a47032aef80792fe2c485fc273c573548
Leonardo <lecastro16@gmail.com>
2021-01-14 12:11:21 -0300

    requisitos 1 ao 3 v9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
 
+
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;  
