c3f0efcbfd33a8f0e0f3330940604faa34a7781e
Leandro Lachini <leandrolachini@gmail.com>
2021-01-14 14:47:52 -0300

    projeto finalizado_correçao codigo_3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -21,11 +21,13 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  movie: {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }
 };
 
 export default MovieCard;
