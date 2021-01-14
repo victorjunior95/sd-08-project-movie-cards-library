8a4f2ee62ce594d31d765d4a8423ea8f0a496b5d
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-01-14 13:13:35 -0300

    final: projeto concluido - dependencia entre os requisitos, tornou a implementacao em testes o motor pra esse grande commit com todos os componentes melhores estratégias serão consultadas para melhor organizar esses commits dentro de React

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,34 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+          <Rating rating={ rating } />
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
+
+export default MovieCard;
