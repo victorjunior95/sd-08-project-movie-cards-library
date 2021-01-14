45c24f615196f27cfd578258981ebe9e790dd146
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-01-13 21:52:12 -0300

    finish all project

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
+
 
 class MovieCard extends React.Component {
   render() {
@@ -22,5 +24,15 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
+
 
 class MovieList extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <section className="movie-list">
-        {movie.map((movie) => (
-          <MovieCard movie={ movie } key={ movie.title } />
-        ))}
-      </section>
+      <div className="movie-list">
+        {movie.map((movies) => (<MovieCard key={ movies.title } movie={ movies } />))}
+      </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // https://www.npmjs.com/package/arrayof
+};
 
 export default MovieList;
