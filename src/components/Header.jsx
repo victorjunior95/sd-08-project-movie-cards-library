3ad1a02d34a2b13e4277d427994b80cca2e2dfcf
Patrícia <silva.patricia.v@gmail.com>
2021-01-15 11:54:33 -0300

    requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
-// Iniciando o projeto 🚀" > src/components/Header.jsx
+import React from 'react';
+
+class Header extends React.Component{
+  render() {
+    return(
+      <header className = 'movie-card-header'>
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
+  }
+}
+export default Header;
+
+
+
