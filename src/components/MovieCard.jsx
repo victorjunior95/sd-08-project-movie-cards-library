2bd069ae647509c353c6318add8e5522b0bc5525
Fernanda Marques <nandateixmarques@gmail.com>
2021-01-16 19:25:59 -0300

    arrumando o requisitos pra passar

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
@@ -20,5 +21,15 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.proptype = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
