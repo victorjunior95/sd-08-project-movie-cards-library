aa4a66ffde28bf8b94fc841ac4c3547fb72838e6
unknown <rosieleserpa@hotmail.com>
2021-01-16 14:52:19 -0300

    requisito 1: crie o componente <Header />

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 
 import App from './App';
-// import Header from './components/Header';
+import Header from './components/Header';
 // import MovieCard from './components/MovieCard';
 // import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
