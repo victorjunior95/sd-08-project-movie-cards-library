3134223616374a245863aedd5c186a666486135e
Fernanda Marques <nandateixmarques@gmail.com>
2021-01-16 18:37:12 -0300

    arrumando os requisito 2, 3, 4 e 5

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Rating extends React.Component {
+class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
-      <p>{ rating }</p>
+      <div>
+        <p>{rating}</p>
+      </div>
     );
   }
 }
