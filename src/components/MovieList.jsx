d48b0a0455028a17dc284853bc0d87a8155cc8a7
Uilton Rocha <itoclick@gmail.com>
2021-01-15 22:11:49 -0300

    Testando Pontuação

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div className="movie-list">
+        {movies.map((card) => <MovieCard key={ card.title } movie={ card } />)}
+      </div>
+    );
+  }
+}
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React from 'react';
+
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>99</h1>
+      </div>
+    );
+  }
+}
+export default Rating;
