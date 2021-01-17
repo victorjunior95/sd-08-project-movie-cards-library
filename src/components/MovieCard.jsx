7b378020b81eb144021102d54b94100a9d25fbb0
Thiago Ferreira <thiagocristhian@gmail.com>
2021-01-17 14:22:44 -0300

    removendo required de prop-types MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -19,11 +19,11 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  rating: PropTypes.number,
 }.isRequired;
 
 export default MovieCard;
