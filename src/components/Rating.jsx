590522545c71f882827ce5b0b2fc70bb7b404e49
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-01-18 17:38:28 -0300

    Criaçao dos arquivos iniciais

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
+//
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
@@ -9,7 +9,7 @@ class Rating extends React.Component {
       <div className="rating">
         {rating}
       </div>
-    ); 
+    );
   }
 }
 
