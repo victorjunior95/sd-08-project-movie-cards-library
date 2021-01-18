f3f20e5943bce13710d2b348bb73aa162dbda1ee
Hugo Andrade Braga <hugobrr@gmail.com>
2021-01-18 14:57:13 -0300

    salva eu Leticia

--- .history/src/components/Header_20210113142452.jsx
+++
@@ -1,9 +0,0 @@
-// implement Header component here
-import React from 'react';
-
-class Header extends React.Component {
-    render() {
-    }
-}
-
-export default Header;
--- .history/src/components/MovieCard_20210113165211.jsx
+++
@@ -1,9 +0,0 @@
-// implement MovieCard component here
-import React from 'react';
-
-class MovieCard extends React.Component {
-render() {
-    return (<h1>testes</h1>)
-}
-}
-export default MovieCard;
