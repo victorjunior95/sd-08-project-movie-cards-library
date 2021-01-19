201b9359fd5f651526d07803fe32fd8d94f557a3
Antônio Schappo <antonioschappo@gmail.com>
2021-01-18 22:43:25 -0300

    First try

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const {rating} = this.props;
+    return <div className="rating">{rating}</div>;
+  }
+}
+
+export default Rating;
+
+// 4.2 - Renderize a nota de um filme dentro de <Rating />
+// <Rating /> deve renderizar a nota do filme recebido na prop rating dentro de um elemento com a classe rating.
+
+// O que será verificado:
+// O componente <Rating /> é renderizado dentro de um elemento com a classe rating.
