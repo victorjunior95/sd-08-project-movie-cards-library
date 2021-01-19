63d48c92aca2b38a97a82715026ebb045febfe7f
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-19 14:49:50 -0300

    C1 - Adjust App.js (create class App)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Hora de codar!! 🚀✍️👨‍💻👩‍💻💪
+      </div>
+    );
+  }
 }
 
 export default App;
