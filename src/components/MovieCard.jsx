305bb6ce841c9ba59aac95afaba81a75928f3437
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-01-19 20:08:16 -0300

    reenvia req2 e envia demais reqs

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,33 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+    render() {
+        const { movie } = this.props;
+        const { title, subtitle, storyline, rating, imagePath } = movie;
+        return (
+            <section className="movie-card">
+                <Rating rating = { rating } />
+                <div>
+                    <h4>{ title }</h4>
+                    <h5>{ subtitle }</h5>
+                    <p>{ storyline }</p>
+                </div>
+                <img src={ imagePath } alt={ title } />
+            </section>
+        );
+    }
+}
+
+MovieCard.propTypes = {
+    movie: {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        imagePath: PropTypes.string.isRequired,
+    }.isRequired,
+};
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+    render() {
+        const { rating } = this.props;
+        return (
+            <div>
+                <section>{ rating }</section>
+            </div>
+        );
+    }
+}
+
+Rating.PropTypes = {
+    rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
