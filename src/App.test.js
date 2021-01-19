0a526e8d209118248f65d21dfee8b7eeb812457d
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-01-19 19:23:27 -0300

    descomentando import do MovieCard no arq App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { mount, shallow } from 'enzyme';
 
 import App from './App';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
+import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 
