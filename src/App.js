6dcf12c80a7d525fd7ec551395741ef2404c8ea4
MuriloBatista <mub4si@gmail.com>
2021-01-19 12:59:18 -0300

    Req:02

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import './App.css';
 import Header from './components/Header';
+
+import './App.css';
+import MovieList from './components/MovieList';
+import movies from './data';
+
 
 function App() {
   return (
-    <div className="App">
+    <main>
       <Header />
-    </div>
+      <MovieList movies={ movies } />
+    </main>
   );
 }
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <section>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+      </section>
+    );
+  }
+}
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }),
+};
+MovieCard.defaultProps = {
+  movie: null,
+};
+
+export default MovieCard;
