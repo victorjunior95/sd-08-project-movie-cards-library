2fd39cfee156aa7c04ace7f7d71e160f6de0e5e3
wagnerberna <cabelo29007@gmail.com>
2021-01-19 20:56:37 -0300

    propTypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,8 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt="imagePath" />
@@ -21,7 +22,7 @@ class MovieCard extends React.Component {
   }
 }
 
-MovieCard.protoTypes = {
+MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
