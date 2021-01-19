aaa1d1e8b74ad5db2412f6348eebeb0643f01731
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-01-18 23:31:28 -0300

    Fix espacos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,26 @@
 // implement MovieCard component here
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import Rating from './Rating';
 
-import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
-       const { movie } = this.props;
+    const { movie } = this.props;
     return (
       <section>
-        <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title }/>
+        <img
+          className="movie-card-image"
+          src={movie.imagePath}
+          alt={movie.title}
+        />
         <section>
-          <h4 className="movie-card-title">{ movie.title }</h4>
-          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
-          <p className="mivie-card-storyline">{ movie.storyline }</p>
-          </section>        
-        <Rating rating={ movie.rating } />
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="mivie-card-storyline">{movie.storyline}</p>
+        </section>
+        <Rating rating={movie.rating} />
       </section>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,19 @@
 // implement MovieList component here
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import MovieCard from './MovieCard';
 
-import PropTypes from 'prop-types';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <section className="movie-list">
+      <section className="movie-list" >
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie }/>)}
       </section>
     );
-  }
+}
 } 
 
 MovieList.propTypes = {
