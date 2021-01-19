6defec845144942f35407fa7aa43ad55a7cf9d73
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-01-19 20:19:48 -0300

    corrigindo issues

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-    render() {
-        const { rating } = this.props;
-        return (
-            <div>
-                <section>{ rating }</section>
-            </div>
-        );
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+        <div>
+            <section>{ rating }</section>
+        </div>
+    );
+  }
 }
 
-Rating.PropTypes = {
-    rating: PropTypes.number.isRequired,
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
