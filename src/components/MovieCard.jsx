42631bd56587fef1708cc88c1012833b1616ea1f
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-19 17:20:45 -0300

    C8 - Add MovieCard PropTypes. Create MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <section className="movie-card">
 
+        <img src={ imagePath } alt={ title } className="movie-card-image" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+      </section>
+    );
+  }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }),
+};
+
+MovieCard.defaultProps = {
+  movie: null,
+};
 
 export default MovieCard;
