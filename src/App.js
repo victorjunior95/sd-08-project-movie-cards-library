24ade094ff57ea701a9e43bc7138bf8f9f9c463c
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-19 15:32:30 -0300

    C3 - Adjusting 1st Req

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        Hora de codar!! 🚀✍️👨‍💻👩‍💻💪
-      </div>
+      <Header />
     );
   }
 }
