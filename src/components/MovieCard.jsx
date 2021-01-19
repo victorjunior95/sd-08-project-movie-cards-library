94b1593898078b87eac67ef9afa297549535fe13
Rafael Greco <rafaelmgreco@hotmail.com>
2021-01-19 19:49:22 -0300

    corrigindo lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,15 +9,15 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img
-          src={movie.imagePath}
-          alt={movie.title}
+          src={ movie.imagePath }
+          alt={ movie.title }
           className="movie-card-image"
         />
         <div className="movie-card-body">
-          <h4 className="movie-card-title">{movie.title}</h4>
-          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
-          <p className="movie-card-storyline">{movie.storyline}</p>
-          <Rating rating={movie.rating} />
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+          <Rating rating={ movie.rating } />
         </div>
       </div>
     );
