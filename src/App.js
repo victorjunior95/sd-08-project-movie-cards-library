bc8aaa6426c22afcc785222d01c240c450c8b890
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-19 15:45:53 -0300

    C6 - Adjust App.js, Adjust App.test.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
 
+import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import data from './data';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div className='App'>
+        <Header />
+        <MovieList />
+      </div>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 class MovieList extends React.Component {
+  render(){
+    return (
+      <section className='movie-list'>
 
+        
+      </section>
+    );
+  }
 }
 
 export default MovieList;
