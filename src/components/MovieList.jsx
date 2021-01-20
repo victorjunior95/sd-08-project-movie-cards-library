d1167548081e993a9c50210fe7bb99ac5b6485e2
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-01-20 02:03:08 -0300

    Componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,3 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class MovieList extends React.Component {}
+import MovieCard from './MovieCard';
+
+export default class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={ movie.title }
+            movie={ movie }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
