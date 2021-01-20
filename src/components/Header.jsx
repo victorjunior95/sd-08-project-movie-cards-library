5381e70f7d44d87e9cfe5e9f241aae3bb16b7692
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-01-19 23:05:17 -0300

    requisito 1.2 renderizado o texto Movie Cards Library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return
+    return (
+        <header className="movie-card-header">
+          <h1 className="page-title">Movie Cards Library</h1>
+        </header> 
+      ); 
   }
 }
+
 export default Header;
