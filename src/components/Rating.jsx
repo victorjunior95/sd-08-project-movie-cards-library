9ecfdd9e81933d71fd06cbf0c06250765275c6ab
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-01-22 13:43:01 -0300

    Requisite Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,18 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+
+    return (
+      <span>{ rating }</span>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
