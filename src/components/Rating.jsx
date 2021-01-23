6797e2b13e604e279680ac319d3eb651e375303f
Erick-Marinho <erickgaves@gmail.com>
2021-01-23 18:57:25 -0300

    corrigindo requisito 03

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1>eu sou Rating</h1>
+      <h1>{ rating }</h1>
     );
   }
 }
