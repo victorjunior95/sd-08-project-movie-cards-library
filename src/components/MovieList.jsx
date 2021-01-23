2a9de4ba7cbbbdfccaac8f8d0052c711e710375d
Erick-Marinho <erickgaves@gmail.com>
2021-01-23 19:52:22 -0300

    entregando requisitos 05 e 06

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,22 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
-    const movie = movies;
+    const movie = this.props.movies;
     return (
       <div>
         {movie.map((item) => <MovieCard key={ item.title } movie={ item } />)}
       </div>
     );
   }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,9 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className="rating">{ rating }</div>
+      <div className="rating">
+        { rating }
+      </div>
     );
   }
 }
