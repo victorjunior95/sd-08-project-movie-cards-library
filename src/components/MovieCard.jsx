ae889f3d0d6bbea5e8b3e037d815e609c80ca531
Andre Sampaio <andresampaiolm@gmail.com>
2021-01-23 18:56:15 -0300

    rating class added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,9 @@ class MovieCard extends React.Component {
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
         <p>{ movie.storyline }</p>
-        <Rating rating={ movie.rating } />
+        <div className="rating">
+          <Rating rating={ movie.rating } />
+        </div>
       </section>
     );
   }
