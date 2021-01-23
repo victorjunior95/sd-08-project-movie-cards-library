c3c256b38bbc3e04a32dfdaae0d65ad47f51b891
Andre Sampaio <andresampaiolm@gmail.com>
2021-01-23 11:17:43 -0300

    Header h1 modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>This is a header!</h1>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
