0bd2f6dc052ad2420c8e26378abb497748c26262
Bruno <brunocmoraes97@gmail.com>
2021-01-25 09:30:36 -0300

    Requisito 2. Renderiza os cards utilizando os componenters MovieList e MovieCard

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import { mount, shallow } from 'enzyme';
 
 import App from './App';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
+import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 
 let wrapper;
@@ -46,17 +46,17 @@ describe('1 - Crie um componente `<Header />`', () => {
 });
 
 describe('2 - Crie um componente `<MovieList />`', () => {
-  it.skip('2.1 - Renderize o componente `<MovieList />`', () => {
+  it('2.1 - Renderize o componente `<MovieList />`', () => {
     shallow(<MovieList movies={movies} />);
   });
 
-  it.skip('2.2 - Renderize componentes `<MovieCard />` dentro de `MovieList`', () => {
+  it('2.2 - Renderize componentes `<MovieCard />` dentro de `MovieList`', () => {
     wrapper = shallow(<MovieList movies={movies} />);
 
     expect(wrapper.find(MovieCard).length).toEqual(3);
   });
 
-  it.skip('2.3 - Passe uma key para cada `<MovieCard />` renderizado', () => {
+  it('2.3 - Passe uma key para cada `<MovieCard />` renderizado', () => {
     wrapper = mount(<MovieList movies={movies} />);
     const movieCards = wrapper.find(MovieCard);
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        <img src={ movies.imagePath } alt={ movies.title } />
+        <h4>{ movies.title }</h4>
+        <h5>{ movies.subtitle }</h5>
+        <span>{ movies.storyline }</span>
+        <span>{ movies.rating }</span>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
