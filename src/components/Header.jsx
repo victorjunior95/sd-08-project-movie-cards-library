eb6ba30761857cc91178bf82c6666d777b63df37
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-01-26 01:00:59 -0300

    Requisito 1 concluido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
-// Iniciando o projeto 🚀
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Movie Cards Library</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
