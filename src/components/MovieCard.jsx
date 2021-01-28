6e7fd394a8b469a4d6aaa50c48d045496b44f3df
OPedroAzevedo <pvas1996@gmail.com>
2021-01-28 03:37:01 -0300

    Requisito 1 e alguns detalhes!

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h1>MovieCard component</h1>
+    );
+  }
+}
+
+export default MovieCard;
