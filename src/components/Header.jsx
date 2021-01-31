fb8b4b915123619df95e896b5a313938a368e4fe
Uibirá <usbarreto@gmail.com>
2021-01-30 21:35:34 -0300

    eslint R1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return(
-      <header className='movie-card-header'>
-        <h1 className='page-title'>Movie Cards Library</h1>                
-          </header>
+    return (
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>                
+      </header>
     );
   }
 }
