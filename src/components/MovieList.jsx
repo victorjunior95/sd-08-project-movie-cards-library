3ea3c68780f21d600a4db1b0cfb7b5af11df217a
Uibirá <usbarreto@gmail.com>
2021-01-30 21:14:01 -0300

    Commit inicial

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        { movies.map((element) => <MovieCard key={ element.title } movie={ element } />)}
+      </section>
+    );
+  }
+}
+MovieList.propTypes = { movies: PropTypes.arrayOf.isRequired };
+export default MovieList;
