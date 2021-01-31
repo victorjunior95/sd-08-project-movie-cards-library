6c9a04e8e3a1e1d7e2f1d23187119aa3cb880fc7
Uibirá <usbarreto@gmail.com>
2021-01-30 21:29:28 -0300

    Eslint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-    render() {
-        const { rating } = this.props;
-        return(
-            <div className='movie-card-rating'>
-                <span className='rating'>{rating}</span>
-            </div>
-        )
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <span className="rating">{rating}</span>
+      </div>
+    );
+  }
 }
 
 Rating.propTypes = { rating: PropTypes.number.isRequired };
