a350c6e5972d31e23fa50837ae97d654c4a5a57c
Max <gmaxgomes@gmail.com>
2021-02-01 00:04:58 -0300

    fixing errors

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { imagePath, title, subtitle, storyline, rating } = this.props.movie;
+		const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline, rating, movie } = movie;
     return (
       <div>
         <img src={ imagePath } alt="" />
@@ -18,11 +19,13 @@ class MovieCard extends Component {
 }
 
 MovieCard.propTypes = {
-	imagePath: PropTypes.string,
-	title: PropTypes.string,
-	subtitle: PropTypes.string,
-	storyline: PropTypes.string,
-	rating: PropTypes.number,
+	movie: PropTypes.shape({
+		title: PropTypes.string,
+		subtitle: PropTypes.string,
+		storyline: PropTypes.string,
+		rating: PropTypes.number,
+		imagePath: PropTypes.string,
+	}).isRequired,
 };
 
 export default MovieCard;
