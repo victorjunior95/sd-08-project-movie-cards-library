4ce80551dce50bcb8cc5e98882205c2d3781a2f4
Max <gmaxgomes@gmail.com>
2021-01-31 23:17:23 -0300

    req 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
 	render() {
+		const movie = this.props.movie;
 		return (
-			<img src={this.props.movie.imagePath} alt=""/>
-    );
+			<div>
+			<img src={ movie.imagePath } alt=""/>
+			<h4>{ movie.title }</h4>
+			<h5>{ movie.subtitle }</h5>
+			<p>{ movie.storyline }</p>
+			<Rating rating={ movie.rating } />
+			</div>
+		);
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import MovieCard from "./MovieCard";
-import movies from '../data';
+import movies from "../data";
 
 class MovieLIst extends React.Component {
   render() {
-		return (
-			movies.map(movie => <MovieCard key={ movie.title } movie = { movie }/>)
-		);
+    return movies.map((movie) => <MovieCard key={movie.title} movie={movie} />);
   }
 }
 
