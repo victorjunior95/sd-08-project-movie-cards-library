70489b63b6dc8310eba009e49819fef2b7debe13
Pedro Alves <ped.alm2709@gmail.com>
2021-02-02 18:21:41 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieCard from './components/MovieCard';
+// import MovieCard from './components/MovieCard';
 
 function App() {
   return (
