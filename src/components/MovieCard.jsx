4ab493097494b3b6911acfb70c2b89ed5878c59a
Lucas Duque <lucas3rios2014@gmail.com>
2021-02-03 18:20:11 -0300

    Requisito 1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+// import React from 'react';
+
+// class MovieCard extends Component {
+//   render() { 
+//     return (
+
+//     );
+//   }
+// }
+ 
+// export default Movie
