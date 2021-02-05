4ffdc4f173f09f9886095097b40c51522b3b7f0f
siebenei5 <siebenei7@gmail.com>
2021-02-05 19:37:55 +0000

    moviecards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
-// implement MovieCard component here
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <section className="movie-card-body">
+        <img src={ imagePath } alt="imagem do filme" className="movie-card-image" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating className="movie-card-rating" rating={ rating } />
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+/* eslint-disable react/prop-types */
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { movies } = this.props;
+
+    return (
+      <section className="movie-list">
+        {movies.map((each) => <MovieCard className="movie-card" key={ each.title } movie={ each } />)}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
