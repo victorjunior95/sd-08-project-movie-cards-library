520dde0dd0a53e5711961da9db180750c5a26142
Víctor Cançado <victor.cancado@gmail.com>
2021-02-05 23:46:52 -0300

    Finaliza

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,18 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1>Olá!</h1>
+      <h1 className="rating">{ rating }</h1>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
