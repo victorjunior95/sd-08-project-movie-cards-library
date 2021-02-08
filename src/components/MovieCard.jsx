a1bda2a0e9cd3584a3b1ff72800284f8725ffab6
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-02-08 02:30:38 -0300

    Requisitos 2 e 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <div>
+        <h3>{ title }</h3>
+        <p>{ subtitle }</p>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+        <img src={ imagePath } alt={ title } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard movie={ movie } key={ movie.id } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
