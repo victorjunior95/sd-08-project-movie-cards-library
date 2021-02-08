8fc2564c4b93e43bd03eb9733371a3caad09c76a
vicoliveir <victorhugoqxd@gmail.com>
2021-02-08 02:20:30 -0300

    sending project eslint factored

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <div className="movie-card">
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4 className="movie-title">{ movie.title }</h4>
+        <h5 className="movie-subtitle">{ movie.subtitle }</h5>
+        <p className="movie-story">{ movie.storyline }</p>
+        <Rating rating={ movie.rating } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = { movie: PropTypes.objectOf.isRequired };
+
+export default MovieCard;
