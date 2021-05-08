56ff0451bd96c10a70a512de4510d9650ee9b10d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-05-08 20:21:36 -0300

    Requisito 05

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <h1>Movie List</h1>
+    );
+  }
+}
+
+export default MovieList;
