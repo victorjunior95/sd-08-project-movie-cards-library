5ec008b72feb9226af6b498d850d53825a1df575
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-05-08 20:50:27 -0300

    Requisito 02

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <h1>{movie.title}</h1>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return (
-      <h1>Movie List</h1>
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title} movie={ movie } />)}
+      </div>
     );
   }
 }
