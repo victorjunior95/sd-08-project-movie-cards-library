710640d3b3d9191e89b277d2ec7f67410228189d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-05-08 21:20:24 -0300

    Requisito 06

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
@@ -15,5 +16,15 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
